6533c6acb7eb86b2cd0ae8ccdbe748ecc0f006f4
garima-203 <162180929+garima-203@users.noreply.github.com>
2025-01-01 22:36:33 +0530

    Added Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'; 
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa'; 
+import CustomCursor from './Components/CustomCursor';
+import Header from './Components/HeaderPage';
+import SocialLink from './Components/SocialLinks';
 function App() {
-  const [count, setCount] = useState(0)
-
+  const navigate = useNavigate();
+  const location = useLocation(); 
+  const pages = ['', 'about', 'skills', 'project', 'contact'];
+ 
+  const currentPageIndex = pages.indexOf(location.pathname.replace('/', ''));
+  const goToNextPage = () => {
+    if (currentPageIndex < pages.length - 1) {
+      navigate(`/${pages[currentPageIndex + 1]}`);
+    } else {
+      navigate(`/${pages[0]}`); 
+    }
+  };
+  const goToPreviousPage = () => {
+    if (currentPageIndex > 0) {
+      navigate(`/${pages[currentPageIndex - 1]}`);
+    } else {
+      navigate(`/${pages[pages.length - 1]}`); 
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <> 
+     <CustomCursor/>
+      <Header/>
+     <SocialLink/>
+      <Outlet /> 
+      <footer className="text-center text-white">
+ © 2025 Designed and Developed by Garima Upadhyay. All rights reserved.
+</footer>
 
-export default App
+      <div className='gap-10'> 
+        <button
+         className="right-1/2 bottom-5 z-50 fixed flex justify-center items-center bg-black shadow-md mx-5 rounded-full w-12 h-12 text-white transform -translate-x-1/2 animate-bounce-right hover:scale-110"
+         onClick={goToPreviousPage}
+          aria-label="Go to Previous Page"
+        >
+          <FaArrowLeft />
+        </button>  
+
+ 
+        <button
+          className="bottom-5 left-1/2 z-50 fixed flex justify-center items-center bg-black shadow-md mx-5 rounded-full w-12 h-12 text-white transform -translate-x-1/2 animate-bounce-right hover:scale-110"
+          onClick={goToNextPage}
+          aria-label="Go to Next Page"
+        >
+          <FaArrowRight />
+        </button>
+      </div> 
+    </>
+  );
+}
+export default App;
