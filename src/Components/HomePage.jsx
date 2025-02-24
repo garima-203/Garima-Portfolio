73c5b096849c011203963d39cf568247bd38b67d
Garima Upadhyay <garimau315@gmail.com>
2025-02-24 18:15:45 +0530

    Resume updated

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -45,7 +45,7 @@ const Home = () => {
                                     Save It
                                 </>
                             }
-                            link="https://drive.google.com/file/d/1iMbhiVM2o29PPjb2schcU-x7BIESNzPW/view?usp=drive_link"
+                            link="https://drive.google.com/file/d/1uWkXWGwv4LVIq6x8p13MNreFUrJJSBlY/view?usp=drive_link"
                         />
                     </div>
                 </div>
